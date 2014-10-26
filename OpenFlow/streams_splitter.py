29aa872ed4ea01474890eac8b3a692c27845ffe2
Oleg Sadov <sadov@linux-ink.ru>
2014-10-26 12:56:04 +0400

    Added OpenFlow directry
    	new file:   OpenFlow/README.md
    	new file:   OpenFlow/of_del
    	new file:   OpenFlow/of_status
    	new file:   OpenFlow/port_watch
    	new file:   OpenFlow/streams_monitor
    	new file:   OpenFlow/streams_splitter.py
    	new file:   OpenFlow/streams_splitter_topo.1.1
    	new file:   OpenFlow/streams_splitter_topo.1.2
    	new file:   OpenFlow/streams_splitter_topo.2.1
    	new file:   OpenFlow/streams_splitter_topo.2.2
    	new file:   OpenFlow/test_net.py
    	new file:   OpenFlow/topo_test

---
+++ OpenFlow/streams_splitter.py
@@ -0,0 +1,170 @@
+#!/bin/env python
+
+# Copyright (C) 2014 Oleg Sadov
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import requests
+import sys
+import imp
+import os.path
+
+def simple_path(url, sw, inp, outp, priority):
+    url += '/stats/flowentry/add'
+    r = requests.post(url, data="""
+{"dpid":"%d", "priority": %d, "actions": [{"type":"OUTPUT", "port":"%s"}],
+ "match": {"in_port": %s}}
+""" % (sw, priority, outp, inp))
+    return r.status_code
+
+def ip_path(url, sw, inp, outp, priority,
+             nw_src=None, tp_src=None, nw_dst=None, tp_dst=None):
+    url += '/stats/flowentry/add'
+    data = '{"dpid":"%d", "priority": %d, "actions": [{"type":"OUTPUT", "port":"%d"}], "match": {"dl_type": 0x0800, "nw_proto": 6, "in_port": %d' % (sw, priority, outp, inp)
+
+    if nw_src:
+        data += ' ,"nw_src": "%s"' % nw_src
+    if tp_src:
+        data += ' ,"tp_src": "%s"' % tp_src
+    if nw_dst:
+        data += ' ,"nw_dst": "%s"' % nw_dst
+    if tp_dst:
+        data += ' ,"tp_dst": "%s"' % tp_dst
+
+    data += '}}'
+
+    print data
+
+    r = requests.post(url, data)
+    return r.status_code
+
+def del_flow(url, sw, inp, priority,
+             nw_src=None, tp_src=None,
+             nw_dst=None, tp_dst=None):
+    url += '/stats/flowentry/delete_strict'
+    data = '{"dpid":"%d", "priority": %d, "match": {"dl_type": 0x0800, "nw_proto": 6, "in_port": %d' % (sw, priority, inp)
+
+    if nw_src:
+        data += ' ,"nw_src": "%s"' % nw_src
+    if tp_src:
+        data += ' ,"tp_src": "%s"' % tp_src
+    if nw_dst:
+        data += ' ,"nw_dst": "%s"' % nw_dst
+    if tp_dst:
+        data += ' ,"tp_dst": "%s"' % tp_dst
+
+    data += '}}'
+
+    print data
+
+    r = requests.post(url, data)
+    return r.status_code
+
+def basic_topo(url, priority, path):
+    for sw in path.keys():
+        if simple_path(url, sw, path[sw][0], path[sw][1], priority) != 200:
+            return 1
+        if simple_path(url, sw, path[sw][1], path[sw][0], priority) != 200:
+            return 1
+
+    return 0
+
+def path(url, priority, path, nw_src=None, tp_src=None,
+         nw_dst=None, tp_dst=None):
+    for sw in path.keys():
+        if ip_path(url, sw, path[sw][0], path[sw][1], priority, \
+                       nw_src, tp_src, nw_dst, tp_dst) != 200:
+            return 1
+        if ip_path(url, sw, path[sw][1], path[sw][0], priority, \
+                       nw_dst, tp_dst, nw_src, tp_src) != 200:
+            return 1
+
+    return 0
+
+def del_path(url, priority, path, nw_src=None, tp_src=None,
+         nw_dst=None, tp_dst=None):
+    for sw in path.keys():
+        if del_flow(url, sw, path[sw][0], priority, \
+                       nw_src, tp_src, nw_dst, tp_dst) != 200:
+            return 1
+        if del_flow(url, sw, path[sw][1], priority, \
+                       nw_dst, tp_dst, nw_src, tp_src) != 200:
+            return 1
+
+    return 0
+
+def monitor(url, priority, paths=[],
+            topo_file='/tmp/streams_splitter_topo.py',
+            streams_file='/tmp/streams_splitter'):
+    flows = []
+    topo_time = 0
+    while 1:
+        t_time = os.path.getmtime(topo_file)
+        if topo_time != t_time:
+            topo = imp.load_source('', topo_file)
+            for sw in topo.clean:
+                requests.delete(url + '/stats/flowentry/clear/%d' % sw)
+            if basic_topo(url, 3301, topo.base):
+                print ":("
+                sys.exit(-1)
+            paths = topo.paths
+            print "new topo: "
+            print "clean=" + `topo.clean`
+            print "base=" + `topo.base`
+            print "paths=" + `topo.paths`
+
+            topo_time = t_time
+            flows = []
+
+        f=open(streams_file,'r')
+        n = len(paths)
+        i = 0
+        tmp_flows = []
+        # Add new flows
+        for s in f.readlines():
+            tmp_flows.append(s)
+            if s not in flows:
+                src, dst = s.split()
+                nw_src, tp_src = src.split(':')[-2:]
+                nw_dst, tp_dst = dst.split(':')[-2:]
+                if path(url, priority, paths[i],
+                        nw_src, tp_src, nw_dst, tp_dst):
+                    print ":(("
+                    sys.exit(-1)
+                i += 1
+                if i >= n:
+                    i = 0
+        # Remove old flows
+        for s in flows:
+            if s not in tmp_flows:
+                src, dst = s.split()
+                nw_src, tp_src = src.split(':')[-2:]
+                nw_dst, tp_dst = dst.split(':')[-2:]
+                if del_path(url, priority, paths[i],
+                            nw_src, tp_src, nw_dst, tp_dst):
+                    print ":(("
+                    sys.exit(-1)
+        flows = tmp_flows
+        f.close()
+
+
+if __name__ == "__main__":
+
+    if len(sys.argv) < 2:
+        print "Usage: %s OF_controller_url"
+        sys.exit(-1)
+    monitor(sys.argv[1], 3331)
+
+    print ":)"
+    sys.exit(0)
---
+++ OpenFlow/test_net.py
@@ -0,0 +1,108 @@
+#!/usr/bin/python
+
+"""
+This example shows how to create an empty Mininet object
+(without a topology object) and add nodes to it manually.
+"""
+
+import sys
+
+from mininet.net import Mininet
+from mininet.node import Controller
+from mininet.cli import CLI
+from mininet.log import setLogLevel, info
+from mininet.node import RemoteController
+from mininet.node import Controller
+from mininet.node import Node
+from mininet.link import Link
+from mininet.log import lg
+
+def emptyNet():
+
+    "Create an empty network and add nodes to it."
+
+    #net = Mininet( controller=Controller )
+    net = Mininet( controller=RemoteController )
+
+    info( '*** Adding controller\n' )
+    net.addController( 'c0' )
+
+    info( '*** Adding hosts\n' )
+    h1 = net.addHost( 'h1', ip='10.0.0.1' )
+    h2 = net.addHost( 'h2', ip='10.0.0.2' )
+
+    info( '*** Adding switch\n' )
+    s3 = net.addSwitch( 's3' )
+    s4 = net.addSwitch( 's4' )
+    s5 = net.addSwitch( 's5' )
+    s6 = net.addSwitch( 's6' )
+
+    info( '*** Creating links\n' )
+    net.addLink( h1, s3 )
+    net.addLink( s3, s4 )
+    net.addLink( s3, s5 )
+    net.addLink( s4, s6 )
+    net.addLink( s5, s6 )
+    net.addLink( h2, s6 )
+
+    print "Stopping of " + 's5'
+    net.configLinkStatus('s5', 's3', 'down')
+    #net.configLinkStatus('s5', 's6', 'down')
+    #net.hosts[0].cmd("ping -w 15 10.0.0.2")
+    #net.pingAll()
+
+    return net
+
+def connectToRootNS( network, switch, ip, routes ):
+    """Connect hosts to root namespace via switch. Starts network.
+      network: Mininet() network object
+      switch: switch to connect to root namespace
+      ip: IP address for root namespace node
+      routes: host networks to route to"""
+    # Create a node in root namespace and link to switch 0
+    root = Node( 'root', inNamespace=False )
+    intf = Link( root, switch ).intf1
+    root.setIP( ip, intf=intf )
+    # Start network that now includes link to root namespace
+    network.start()
+    # Add routes from root ns to hosts
+    for route in routes:
+        root.cmd( 'route add -net ' + route + ' dev ' + str( intf ) )
+
+def sshd( network, cmd='/usr/sbin/sshd', opts='-D',
+          ip='10.123.123.1/32', routes=None, switch=None ):
+    """Start a network, connect it to root ns, and run sshd on all hosts.
+       ip: root-eth0 IP address in root namespace (10.123.123.1/32)
+       routes: Mininet host networks to route to (10.0/24)
+       switch: Mininet switch to connect to root namespace (s1)"""
+    if not switch:
+        switch = network[ 's3' ]  # switch to use
+    if not routes:
+         routes = [ '10.0.0.0/24' ]
+    connectToRootNS( network, switch, ip, routes )
+    for host in network.hosts:
+        host.cmd( cmd + ' ' + opts + '&' )
+    print
+    print "*** Hosts are running sshd at the following addresses:"
+    print
+    for host in network.hosts:
+        print host.name, host.IP()
+    print
+    print "*** Type 'exit' or control-D to shut down network"
+    CLI( network )
+    for host in network.hosts:
+        host.cmd( 'kill %' + cmd )
+    network.stop()
+
+if __name__ == '__main__':
+    #lg.setLogLevel( 'info')
+    lg.setLogLevel( 'debug')
+    net = emptyNet() #TreeNet( depth=1, fanout=4 )
+    # get sshd args from the command line or use default args
+    # useDNS=no -u0 to avoid reverse DNS lookup timeout
+    opts = ' '.join( sys.argv[ 1: ] ) if len( sys.argv ) > 1 else (
+        '-D -o UseDNS=no -u0' )
+    sshd( net, opts=opts )
+    #CLI( net)
+    #net.stop()
+   
