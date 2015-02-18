72d371e14d3939f4d5e8cd793fabc1a6bcbdc459
Elena Korytko <elena.korytko@gmail.com>
2015-02-18 11:31:42 +0300

    Added sort
    	modified:   gnuplot.sh

--- plot/gnuplot.sh
+++ plot/gnuplot.sh
@@ -5,7 +5,7 @@
 
 for i in 1 2 3 4 18 26 50 64 
 do
-	grep -r 'Average transfer speed' $TEST.$DATE/s$i | awk '{print NR, $6}' > DataSpeed-$TEST.s$i
+	grep -r 'Average transfer speed' $TEST.$DATE/s$i | sort | awk '{print NR, $6}' > DataSpeed-$TEST.s$i
 done
 
 ./DataSpeed.plot.sh 2>&1 | tee DataSpeed-$TEST.$DATE.plot | gnuplot > DataSpeed-$TEST.$DATE.ps; convert DataSpeed-$TEST.$DATE.ps DataSpeed-$TEST.$DATE.pdf; convert DataSpeed-$TEST.$DATE.pdf DataSpeed-$TEST.$DATE.jpg; evince DataSpeed-$TEST.$DATE.pdf; eog DataSpeed-$TEST.$DATE.jpg
